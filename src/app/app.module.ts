98b8b61844765073e2491c0b81260bd9e01f3dcf
Ro18767 <timaropw@gmail.com>
2023-09-15 19:15:12 +0300

    убрал часть с домашкой

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { UserComponent } from './user/user.component';
-import { AvatarComponent } from './user/avatar/avatar.component';
-import { AvatarSettingsComponent } from './user/avatar-settings/avatar-settings.component';
 import { AddComponent } from './catalog/add/add.component';
 import { ListComponent } from './catalog/list/list.component';
 import { DeleteComponent } from './catalog/delete/delete.component';
@@ -17,9 +14,6 @@ import { LoggerService } from './logger.service';
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
-    AvatarComponent,
-    AvatarSettingsComponent,
     AddComponent,
     ListComponent,
     DeleteComponent,
